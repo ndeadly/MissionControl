8802a7b9ee5948e7e1567d15716b926cc2205ecd
ndeadly <24677491+ndeadly@users.noreply.github.com>
2020-07-11 21:11:13 +0200

    btdrv-mitm: forgot to track fakeswitchcontroller

---
+++ btdrv-mitm/source/controllers/fakeswitchcontroller.cpp
@@ -0,0 +1,7 @@
+#include "fakeswitchcontroller.hpp"
+
+namespace ams::controller {
+
+
+
+}
---
+++ btdrv-mitm/source/controllers/fakeswitchcontroller.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "switchcontroller.hpp"
+
+namespace ams::controller {
+
+    class FakeSwitchController : public SwitchController {
+
+        public:
+            FakeSwitchController(ControllerType type, const bluetooth::Address *address) : SwitchController(type, address) {};
+            
+            //readHidData();
+            //writeHidData();
+
+        private:
+
+    };
+
+}
