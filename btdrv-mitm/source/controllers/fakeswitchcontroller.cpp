ec37e65e6983e4e26af9dcb72181af1f1c215573
ndeadly <24677491+ndeadly@users.noreply.github.com>
2020-08-17 22:25:43 +0200

    btdrv-mitm: simplify emulated SPI flash reads to return uninitialised data (0xff) when possible

--- btdrv-mitm/source/controllers/fakeswitchcontroller.cpp
+++ btdrv-mitm/source/controllers/fakeswitchcontroller.cpp
@@ -96,61 +96,31 @@ namespace ams::controller {
     }
 
     Result FakeSwitchController::subCmdSpiFlashRead(const bluetooth::HidReport *report) {
-        // Official controller reads these
-        // @ 0x00006000: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff  <= Serial 
-        // @ 0x00006050: 32 32 32 ff ff ff ff ff ff ff ff ff  <= RGB colours (body, buttons, left grip, right grip)
-        // @ 0x00006080: 50 fd 00 00 c6 0f 0f 30 61 ae 90 d9 d4 14 54 41 15 54 c7 79 9c 33 36 63  <= Factory Sensor and Stick device parameters
-        // @ 0x00006098: 0f 30 61 ae 90 d9 d4 14 54 41 15 54 c7 79 9c 33 36 63  <= Stick device parameters 2. Normally the same with 1, even in Pro Contr.
-        // @ 0x00008010: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
-        // @ 0x0000603d: e6 a5 67 1a 58 78 50 56 60 1a f8 7f 20 c6 63 d5 15 5e ff 32 32 32 ff ff ff  <= Left analog stick calibration
-        // @ 0x00006020: 64 ff 33 00 b8 01 00 40 00 40 00 40 17 00 d7 ff bd ff 3b 34 3b 34 3b 34  <= 6-Axis motion sensor Factory calibration
+        // Official Pro Controller reads these
+        // @ 0x00006000: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff                            <= Serial 
+        // @ 0x00006050: 32 32 32 ff ff ff ff ff ff ff ff ff                                        <= RGB colours (body, buttons, left grip, right grip)
+        // @ 0x00006080: 50 fd 00 00 c6 0f 0f 30 61 ae 90 d9 d4 14 54 41 15 54 c7 79 9c 33 36 63    <= Factory Sensor and Stick device parameters
+        // @ 0x00006098: 0f 30 61 ae 90 d9 d4 14 54 41 15 54 c7 79 9c 33 36 63                      <= Stick device parameters 2. Normally the same with 1, even in Pro Contr.
+        // @ 0x00008010: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff    <= User Analog sticks calibration
+        // @ 0x0000603d: e6 a5 67 1a 58 78 50 56 60 1a f8 7f 20 c6 63 d5 15 5e ff 32 32 32 ff ff ff <= Left analog stick calibration
+        // @ 0x00006020: 64 ff 33 00 b8 01 00 40 00 40 00 40 17 00 d7 ff bd ff 3b 34 3b 34 3b 34    <= 6-Axis motion sensor Factory calibration
         
         u32 read_addr = *(u32 *)(&report->data[11]);
         u8  read_size = report->data[15];
 
-        if (read_addr == 0x6000 && read_size == 0x10) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
+        const u8 prefix[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15]};
+
+        int response_size = read_size + sizeof(prefix);
+        auto response = std::make_unique<u8[]>(response_size);
+        std::memcpy(response.get(), prefix, sizeof(prefix));
+        std::memset(response.get() + sizeof(prefix), 0xff, read_size); // Console doesn't seem to mind if response is uninitialised data (0xff)
+
+        // Set default controller body colour
+        if (read_addr == 0x6050) {
+            std::memset(response.get() + sizeof(prefix), 0x32, 3);
         }
 
-        else if (read_addr == 0x6050 && read_size == 0x0d) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0x32, 0x32, 0x32, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
-        }
-
-        else if (read_addr == 0x6080 && read_size == 0x18) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0x50, 0xfd, 0x00, 0x00, 0xc6, 0x0f, 0x0f, 0x30, 0x61, 0xae, 0x90, 0xd9, 0xd4, 0x14, 0x54, 0x41, 0x15, 0x54, 0xc7, 0x79, 0x9c, 0x33, 0x36, 0x63};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
-        }
-
-        else if (read_addr == 0x6098 && read_size == 0x12) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0x0f, 0x30, 0x61, 0xae, 0x90, 0xd9, 0xd4, 0x14, 0x54, 0x41, 0x15, 0x54, 0xc7, 0x79, 0x9c, 0x33, 0x36, 0x63};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
-        }
-
-        else if (read_addr == 0x8010 && read_size == 0x18) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
-        }
-
-        else if (read_addr == 0x603d && read_size == 0x19) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0xe6, 0xa5, 0x67, 0x1a, 0x58, 0x78, 0x50, 0x56, 0x60, 0x1a, 0xf8, 0x7f, 0x20, 0xc6, 0x63, 0xd5, 0x15, 0x5e, 0xff, 0x32, 0x32, 0x32, 0xff, 0xff, 0xff};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
-        }
-
-        else if (read_addr == 0x6020 && read_size == 0x18) {
-            const u8 response[] = {0x90, bluetooth::SubCmd_SpiFlashRead, report->data[11], report->data[12], report->data[13], report->data[14], report->data[15],
-                                0x64, 0xff, 0x33, 0x00, 0xb8, 0x01, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x17, 0x00, 0xd7, 0xff, 0xbd, 0xff, 0x3b, 0x34, 0x3b, 0x34, 0x3b, 0x34};
-            R_TRY(bluetooth::hid::report::FakeSubCmdResponse(&m_address, response, sizeof(response)));
-        }
-        
-        return ams::ResultSuccess();
+        return bluetooth::hid::report::FakeSubCmdResponse(&m_address, response.get(), response_size);    
     }
 
     Result FakeSwitchController::subCmdSpiFlashWrite(const bluetooth::HidReport *report) {
