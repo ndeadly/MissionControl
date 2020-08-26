165d2e87b3137fed26ea17f59c7029519a42a7d1
ndeadly <24677491+ndeadly@users.noreply.github.com>
2020-08-26 21:24:20 +0200

    Stop tracking old code directories

--- sysmodule/include/gamepad/controllers.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "gamepad/controllers/joycon.hpp"
-#include "gamepad/controllers/switchpro.hpp"
-#include "gamepad/controllers/wiimote.hpp"
-#include "gamepad/controllers/wiiupro.hpp"
-#include "gamepad/controllers/dualshock4.hpp"
-#include "gamepad/controllers/xboxone.hpp"
