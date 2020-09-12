8b6a733bbac116e8b699d934dfe5eff5cb70fe69
ndeadly <24677491+ndeadly@users.noreply.github.com>
2020-09-12 16:20:00 +0200

    bluetooth-mitm: add button mappings for gamesir controller

--- bluetooth-mitm/source/controllers/gamesir_controller.cpp
+++ bluetooth-mitm/source/controllers/gamesir_controller.cpp
@@ -21,6 +21,7 @@ namespace ams::controller {
 
     namespace {
 
+        const constexpr float stick_scale_factor = float(UINT12_MAX) / UINT8_MAX;
 
     }
 
@@ -29,6 +30,12 @@ namespace ams::controller {
         auto switch_report = reinterpret_cast<SwitchReportData *>(&out_report->data);
 
         switch(gamesir_report->id) {
+            case 0x12:
+                this->HandleInputReport0x12(gamesir_report, switch_report);
+                break;
+            case 0xc4:
+                this->HandleInputReport0xc4(gamesir_report, switch_report);
+                break;
             default:
                 break;
         }
@@ -39,6 +46,55 @@ namespace ams::controller {
         switch_report->input0x30.battery = m_battery | m_charging;
         std::memset(switch_report->input0x30.motion, 0, sizeof(switch_report->input0x30.motion));
         switch_report->input0x30.timer = os::ConvertToTimeSpan(os::GetSystemTick()).GetMilliSeconds() & 0xff;
+    }
+
+    void GamesirController::HandleInputReport0x12(const GamesirReportData *src, SwitchReportData *dst) {
+        dst->input0x30.buttons.home = src->input0x12.home;
+
+        this->PackStickData(&dst->input0x30.left_stick, STICK_ZERO, STICK_ZERO);
+        this->PackStickData(&dst->input0x30.right_stick, STICK_ZERO, STICK_ZERO);
+    }
+
+    void GamesirController::HandleInputReport0xc4(const GamesirReportData *src, SwitchReportData *dst) {
+        this->PackStickData(&dst->input0x30.left_stick,
+            static_cast<uint16_t>(stick_scale_factor * src->input0xc4.left_stick.x) & 0xfff,
+            static_cast<uint16_t>(stick_scale_factor * (UINT8_MAX - src->input0xc4.left_stick.y)) & 0xfff
+        );
+        this->PackStickData(&dst->input0x30.right_stick,
+            static_cast<uint16_t>(stick_scale_factor * src->input0xc4.right_stick.x) & 0xfff,
+            static_cast<uint16_t>(stick_scale_factor * (UINT8_MAX - src->input0xc4.right_stick.y)) & 0xfff
+        );
+
+        dst->input0x30.buttons.dpad_down   = (src->input0xc4.buttons.dpad == GamesirDpad_S)  ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_SE) ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_SW);
+        dst->input0x30.buttons.dpad_up     = (src->input0xc4.buttons.dpad == GamesirDpad_N)  ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_NE) ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_NW);
+        dst->input0x30.buttons.dpad_right  = (src->input0xc4.buttons.dpad == GamesirDpad_E)  ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_NE) ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_SE);
+        dst->input0x30.buttons.dpad_left   = (src->input0xc4.buttons.dpad == GamesirDpad_W)  ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_NW) ||
+                                             (src->input0xc4.buttons.dpad == GamesirDpad_SW);
+
+        dst->input0x30.buttons.A = src->input0xc4.buttons.B;
+        dst->input0x30.buttons.B = src->input0xc4.buttons.A;
+        dst->input0x30.buttons.X = src->input0xc4.buttons.Y;
+        dst->input0x30.buttons.Y = src->input0xc4.buttons.X;
+
+        dst->input0x30.buttons.R  = src->input0xc4.buttons.RB;
+        dst->input0x30.buttons.ZR = src->input0xc4.buttons.RT;
+        dst->input0x30.buttons.L  = src->input0xc4.buttons.LB;
+        dst->input0x30.buttons.ZL = src->input0xc4.buttons.LT; 
+
+        dst->input0x30.buttons.minus = src->input0xc4.buttons.select;
+        dst->input0x30.buttons.plus  = src->input0xc4.buttons.start;
+
+        dst->input0x30.buttons.lstick_press = src->input0xc4.buttons.L3;
+        dst->input0x30.buttons.rstick_press = src->input0xc4.buttons.R3;  
+
+        dst->input0x30.buttons.capture  = 0;
     }
 
 }
--- bluetooth-mitm/source/controllers/gamesir_controller.hpp
+++ bluetooth-mitm/source/controllers/gamesir_controller.hpp
@@ -19,8 +19,68 @@
 
 namespace ams::controller {
 
+    enum GamesirDpadDirection {
+        GamesirDpad_Released,
+        GamesirDpad_N,
+        GamesirDpad_NE,
+        GamesirDpad_E,
+        GamesirDpad_SE,
+        GamesirDpad_S,
+        GamesirDpad_SW,
+        GamesirDpad_W,
+        GamesirDpad_NW,
+    };
+
+    struct GamesirStickData {
+        uint8_t x;
+        uint8_t y;
+    } __attribute__((packed));
+
+    struct GamesirButtonData {
+        uint8_t A       : 1;
+        uint8_t B       : 1;
+        uint8_t         : 1;
+        uint8_t X       : 1;
+        uint8_t Y       : 1;
+        uint8_t         : 1;
+        uint8_t LB      : 1;
+        uint8_t RB      : 1;
+
+        uint8_t LT      : 1;
+        uint8_t RT      : 1;
+        uint8_t select  : 1;
+        uint8_t start   : 1;
+        uint8_t         : 1;
+        uint8_t L3      : 1;
+        uint8_t R3      : 1;
+        uint8_t         : 0;
+
+        uint8_t dpad;
+    } __attribute__((packed));
+
+    struct GamesirReport0x12 {
+        uint8_t         : 3;
+        uint8_t home    : 1;
+        uint8_t         : 0;
+
+        uint8_t _unk[2];
+    } __attribute__((packed));
+
+    struct GamesirReport0xc4 {
+        GamesirStickData    left_stick;
+        GamesirStickData    right_stick;
+        uint8_t             left_trigger;
+        uint8_t             right_trigger;
+        GamesirButtonData   buttons;
+        uint8_t             _unk;
+    } __attribute__((packed));
+
     struct GamesirReportData {
         uint8_t id;
+        union {
+            GamesirReport0x12  input0x12;
+            GamesirReport0xc4  input0xc4;
+        };
     } __attribute__((packed));
 
     class GamesirController : public EmulatedSwitchController {
@@ -36,6 +96,8 @@ namespace ams::controller {
             void ConvertReportFormat(const bluetooth::HidReport *in_report, bluetooth::HidReport *out_report);
 
         private:
+            void HandleInputReport0x12(const GamesirReportData *src, SwitchReportData *dst);
+            void HandleInputReport0xc4(const GamesirReportData *src, SwitchReportData *dst);
 
     };
 
