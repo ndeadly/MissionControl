3d635cc4682eb1fa5214c63eb1cbb71fced5aa97
ndeadly <24677491+ndeadly@users.noreply.github.com>
2021-01-26 04:03:26 +0100

    Update/fix copyright headers

--- bluetooth-mitm/source/controllers/gembox_controller.cpp
+++ bluetooth-mitm/source/controllers/gembox_controller.cpp
@@ -1,15 +1,14 @@
 /*
- * Copyright (C) 2020 ndeadly
+ * Copyright (c) 2020-2021 ndeadly
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
