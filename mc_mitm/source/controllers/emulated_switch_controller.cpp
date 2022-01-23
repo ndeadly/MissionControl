b5d1936c517fee3d1a6d2ea4b35ecce91cee9238
OperationNT414C <OperationNT414C@users.noreply.github.com>
2022-01-23 18:02:55 +0100

    Update emulated_switch_controller.cpp

--- mc_mitm/source/controllers/emulated_switch_controller.cpp
+++ mc_mitm/source/controllers/emulated_switch_controller.cpp
@@ -202,10 +202,10 @@ namespace ams::controller {
 
         // Apply config modifications
         auto config = mitm::GetGlobalConfig();
-        if (config->general.left_stick_deadzone > 0)
-            m_left_stick.ForceDeadzone(config->general.left_stick_deadzone);
-        if (config->general.right_stick_deadzone > 0)
-            m_right_stick.ForceDeadzone(config->general.right_stick_deadzone);
+        if (config->general.left_stick_deadzone > 0 || config->general.left_stick_outer_deadzone > 0 )
+            m_left_stick.ForceDeadzone(config->general.left_stick_deadzone, config->general.left_stick_outer_deadzone);
+        if (config->general.right_stick_deadzone > 0 || config->general.right_stick_outer_deadzone > 0)
+            m_right_stick.ForceDeadzone(config->general.right_stick_deadzone, config->general.right_stick_outer_deadzone);
 
         // Prepare Switch report
         m_input_report.size = sizeof(SwitchInputReport0x30) + 1;
