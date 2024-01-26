6ff44b5e9c258825581a288ccbc24b5948206013
ndeadly <24677491+ndeadly@users.noreply.github.com>
2024-01-26 10:23:42 +0100

    Update copyright headers

--- mc_mitm/source/controllers/dualshock3_controller.cpp
+++ mc_mitm/source/controllers/dualshock3_controller.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2023 ndeadly
+ * Copyright (c) 2020-2024 ndeadly
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
