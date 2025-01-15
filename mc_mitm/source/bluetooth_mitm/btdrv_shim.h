5f9f70d238e1b4650e3c0d2072894072f3212579
ndeadly <24677491+ndeadly@users.noreply.github.com>
2025-01-15 18:37:23 +0100

    Update copyright headers

--- mc_mitm/source/bluetooth_mitm/btdrv_shim.h
+++ mc_mitm/source/bluetooth_mitm/btdrv_shim.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2023 ndeadly
+ * Copyright (c) 2020-2025 ndeadly
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
--- mc_mitm/source/controllers/dualshock3_controller.cpp
+++ mc_mitm/source/controllers/dualshock3_controller.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024 ndeadly
+ * Copyright (c) 2020-2025 ndeadly
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
