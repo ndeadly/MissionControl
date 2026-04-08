f97acd8059cae0ac04f85f4b30199f2543e745e5
ndeadly <24677491+ndeadly@users.noreply.github.com>
2026-04-08 17:10:56 +0200

    mc.mitm: add missing pragma once directive to module config header
    (cherry picked from commit f61bb7e2074a2a685d1e61fdccd98b81908435ac)

--- mc_mitm/source/mcmitm_config.hpp
+++ mc_mitm/source/mcmitm_config.hpp
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
 #include "bluetooth_mitm/bluetooth/bluetooth_types.hpp"
 
 namespace ams::mitm {
